f6b93ba6584902435d461f7d669dd50701db6f19
Joao Sousa <joao.sousa@sine.co>
2023-01-04 15:15:31 +1030

    chore: add swagger

---
+++ prisma/seed.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const post1 = await prisma.article.upsert({
+    where: { title: 'Prisma Adds Support for MongoDB' },
+    update: {},
+    create: {
+      title: 'Prisma Adds Support for MongoDB',
+      body: 'Support for MongoDB has been one of the most request features.',
+      description:
+        "We are excited to share that today's Prisma ORM release adds stable support for MongoDB!",
+      published: false,
+    },
+  });
+
+  const post2 = await prisma.article.upsert({
+    where: { title: 'Why Prisma?' },
+    update: {},
+    create: {
+      title: 'Why Prisma?',
+      body: 'Support for MongoDB has been one of the most request features.',
+      description:
+        "We are excited to share that today's Prisma ORM release adds stable support for MongoDB!",
+      published: true,
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect;
+  });
